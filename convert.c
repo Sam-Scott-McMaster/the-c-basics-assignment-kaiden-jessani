b0710db3e2a0c2a7b78c6dee6b5aae35ed85191b
kaiden-jessani <jessanik@mcmaster.ca>
2024-11-08 16:09:50 -0500

    Update convert.c

--- convert.c
+++ convert.c
@@ -4,23 +4,47 @@
 #include <stdbool.h>
 #include "convert_to_base.h"
 
+void print_help() {
+    printf("Usage: convert [-b BASE] [-r START FINISH]\n\n");
+    printf("Options:\n");
+    printf("  -b BASE          Specify the base for conversion (default is 16). Valid range is 2 to 36.\n");
+    printf("  -r START FINISH  Convert numbers in the range from START to FINISH (inclusive).\n\n");
+    printf("Description:\n");
+    printf("  This program converts numbers to a specified base. If no arguments are provided, it reads numbers from standard input and converts them to the default base (16). If a base is specified with the `-b` option, it converts numbers to that base. The `-r` option allows converting a range of numbers from START to FINISH.\n\n");
+    printf("Examples:\n");
+    printf("  convert\n");
+    printf("    Reads numbers from standard input and converts them to base 16.\n\n");
+    printf("  convert -b 10\n");
+    printf("    Reads numbers from standard input and converts them to base 10.\n\n");
+    printf("  convert -r 1 10\n");
+    printf("    Converts numbers from 1 to 10 to base 16.\n\n");
+    printf("  convert -b 2 -r 1 10\n");
+    printf("    Converts numbers from 1 to 10 to base 2.\n");
+}
+
 int main(int argc, char *argv[]) {
     long n;
     int base = 16; // Default base
     int min = 0, max = 0;
     bool isNegative = false;
     char sum[65]; // Assuming a maximum size for the sum array
-    if (argc == 1 || argc == 3) {
-        // Handle input for argc == 1 and argc == 3
-        if (argc == 3) {
-            base = atoi(argv[2]);
-            if (base < 2 || base > 36) {
-                fprintf(stderr, "Usage: convert [-b BASE] [-r START FINISH]\n");
-                fprintf(stderr, "\t1 < BASE < 37\n");
-                fprintf(stderr, "\tSTART and FINISH are long integers");
-                return 1;
-            }
+
+    if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+        print_help();
+        return 0;
+    }
+
+    if (argc == 3) {
+        base = atoi(argv[2]);
+        if (base < 2 || base > 36) {
+            fprintf(stderr, "Usage: convert [-b BASE] [-r START FINISH]\n");
+            fprintf(stderr, "\t1 < BASE < 37\n");
+            fprintf(stderr, "\tSTART and FINISH are long integers\n");
+            return 1;
         }
+    }
+
+    if (argc == 1 || argc == 3) {
         while (1) {
             if (scanf("%ld", &n) == EOF) {
                 break; // End the loop if EOF is reached
@@ -45,8 +69,7 @@ int main(int argc, char *argv[]) {
         }
     } else if (argc == 4) {
         fflush(stdout);
-        if(strcmp(argv[1], "-r") == 0)
-        {
+        if (strcmp(argv[1], "-r") == 0) {
             min = atoi(argv[2]);
             max = atoi(argv[3]);
             for (long i = min; i <= max; i++) {
@@ -68,24 +91,20 @@ int main(int argc, char *argv[]) {
                 }
                 isNegative = false;
             }
-        }
-        else
-        {
+        } else {
             fprintf(stderr, "Usage: convert [-b BASE] [-r START FINISH]\n");
             fprintf(stderr, "\t1 < BASE < 37\n");
-            fprintf(stderr, "\tSTART and FINISH are long integers");
+            fprintf(stderr, "\tSTART and FINISH are long integers\n");
             return 1;
         }
-            
-        }
-        else if (argc == 6) {
+    } else if (argc == 6) {
         base = atoi(argv[2]);
         min = atoi(argv[4]);
         max = atoi(argv[5]);
         if (base < 2 || base > 36) {
             fprintf(stderr, "Usage: convert [-b BASE] [-r START FINISH]\n");
             fprintf(stderr, "\t1 < BASE < 37\n");
-            fprintf(stderr, "\tSTART and FINISH are long integers");
+            fprintf(stderr, "\tSTART and FINISH are long integers\n");
             return 1;
         }
         for (long i = min; i <= max; i++) {
@@ -110,7 +129,7 @@ int main(int argc, char *argv[]) {
     } else {
         fprintf(stderr, "Usage: convert [-b BASE] [-r START FINISH]\n");
         fprintf(stderr, "\t1 < BASE < 37\n");
-        fprintf(stderr, "\tSTART and FINISH are long integers");
+        fprintf(stderr, "\tSTART and FINISH are long integers\n");
         return 1;
     }
 
